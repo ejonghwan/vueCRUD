ba5e7f661bada7af6dd2311fdd2996f53392219f
ejonghwan <sun87-1@daum.net>
2024-03-21 18:40:55 +0900

    as

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,50 @@
+
+
 <template>
-  <div id="app">
-    <Header />
-    <div>
-      <router-view></router-view>
-    </div>
+  <div class="hoho">
+    asdasd
   </div>
+  <input type="text" ref="inp">
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
-<script>
-import Header from './components/layout/Header.vue'
 
-export default {
-  name: 'App',
-  components: {
-    Header
-  }
-}
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+import { ref, onBeforeMount, onMounted, onUpdated, onUnmounted } from 'vue';
+
+const inp = ref(null)
+
+onBeforeMount(() => {
+  console.log('onBeforeMount')
+})
+
+onMounted(() => {
+  document.querySelector('.hoho').style.border = '1px solid red';
+  console.log('inp ref??', inp.value)
+  console.log('onMounted')
+
+})
+
+
+
+onUpdated(() => {
+  document.querySelector('.hoho').style.border = '1px solid blue';
+  // console.log('onUpdated')
+  alert('updated?')
+})
+
+
+onUnmounted(() => {
+  document.querySelector('.hoho').style.border = '1px solid yellow';
+  console.log('onUnmounted')
+  alert('unmounted')
+})
+
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
-  /* color: #2c3e50; */
-  margin-top: 60px;
-}
+
+<style scoped>
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
+import './style.css'
 import App from './App.vue'
-import router from './router.js'
-Vue.config.productionTip = false
 
-new Vue({
-  router,
-  render: h => h(App),
-}).$mount('#app')
+createApp(App).mount('#app')
