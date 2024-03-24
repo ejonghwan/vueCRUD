22453cfbb0127d5dde05eaea0547aeb3e0d02572
ejonghwan <sun87-1@daum.net>
2024-03-25 00:45:02 +0900

    8부터

---
+++ src/components/atoms/button/BasicButton.vue
@@ -0,0 +1,58 @@
+<template>
+   <div class="basic-button">
+        <img :src="`src/assets/icons/${data.icon}`" alt="" class="basic-button__icon">
+        <span class="basic-button__label">{{ data.label }}</span>
+   </div>
+</template>
+
+<script lang="ts" setup>
+
+import { toRefs } from 'vue';
+
+interface Button {
+    icon: string;
+    label: string;
+}
+
+interface Props {
+    data: Button
+}
+
+const props = defineProps<Props>()
+const { data } = toRefs(props)
+
+
+</script>
+
+<style lang="scss" scoped>
+
+   .basic-button {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    width: calc(100% - 24px);
+    height: 50px;
+    padding-left: 24px;
+    gap: 16px;
+
+    cursor: pointer;
+
+    &__icon {
+        width: 24px;
+        height: 24px;
+    }
+
+    &__label {
+        padding-top: 4px;
+        font-size: 18px;
+        color: $color-white-000;
+    }
+
+    &:hover {
+        width: 100%;
+        background-color: rgba($color-white-000, 0.1);
+        border-radius: 12px;
+    }
+   }
+
+</style>
--- src/components/organisms/Navigation.vue
+++ src/components/organisms/Navigation.vue
@@ -2,14 +2,20 @@
     
     <BasicLayoutVue :theme="`blue`">
         <div class="navigation">
-            <div class="navigation__sub-menu__button-box"></div>
+            <div class="navigation__sub-menu__button-box">
+                <BasicButton v-for="item in topMenu" :key="item.label" :data="item" />
+            </div>
             <div class="navigation__sub-menu__button-box">
                 <span class="navigation__sub-menu__title">Recruitment</span>
-                <div class="navigation__sub-menu__button-box"></div>
+                <div class="navigation__sub-menu__button-box">
+                    <BasicButton v-for="item in middleMenu" :key="item.label" :data="item" />
+                </div>
             </div>
             <div class="navigation__sub-menu__button-box">
                 <span class="navigation__sub-menu__title">Oraganization</span>
-                <div class="navigation__sub-menu__button-box"></div>
+                <div class="navigation__sub-menu__button-box">
+                    <BasicButton v-for="item in bottomMenu" :key="item.label" :data="item" />
+                </div>
             </div>
         </div>
     </BasicLayoutVue>
@@ -18,6 +24,32 @@
 
 <script setup lang="ts">
 import BasicLayoutVue from "@components/atoms/layout/BasicLayout.vue";
+import { computed } from "vue";
+import BasicButton from "../atoms/button/BasicButton.vue";
+
+const menu = [
+    { icon: 'Dashboard', label: 'Dashboard' },
+    { icon: 'Index', label: 'Index' },
+    { icon: 'Calender', label: 'Calender' },
+    { icon: 'Jobs', label: 'Jobs' },
+    { icon: 'Reference', label: 'Reference' },
+    { icon: 'Document', label: 'Document' },
+    { icon: 'CareerSite', label: 'CareerSite' },
+    { icon: 'Employee', label: 'Employee' },
+    { icon: 'Structure', label: 'Structure' },
+    { icon: 'Settings', label: 'Settings' },
+]
+
+const topMenu = computed(() => {
+    return menu.slice(0, 3)
+})
+const middleMenu = computed(() => {
+    return menu.slice(3, 7)
+})
+const bottomMenu = computed(() => {
+    return menu.slice(7, menu.length)
+})
+
 </script>
 
 <style lang="scss" scoped>
